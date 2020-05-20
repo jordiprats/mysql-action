a11a1908bf61bd462c303559f84a7758000883f5
Jordi Prats <jordi.prats@gmail.com>
2020-05-20 12:10:53 +0200

    rework import script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,10 @@
 
 DOCKERUN="docker run"
 
-echo "$INPUT_MYSQL_ROOT_PASSWORD"
-
-INPUT_MYSQL_ROOT_PASSWORD=${INPUT_MYSQL_ROOT_PASSWORD-sha256}
-
 DOCKERUN="$DOCKERUN -e MYSQL_ROOT_PASSWORD=$INPUT_MYSQL_ROOT_PASSWORD"
+
 echo -e "[client]\npassword=${INPUT_MYSQL_ROOT_PASSWORD-sha256}" > ./docker-mycnf
-
-echo "$INPUT_MYSQL_ROOT_PASSWORD"
-
 chmod 0600 ./docker-mycnf
-
-cat ./docker-mycnf
 
 DOCKERUN="$DOCKERUN -d -p 3307:3307 mysql:$INPUT_MYSQL_VERSION --port=3307"
 
@@ -23,34 +15,11 @@ CONTAINER_ID=$(sh -c "$DOCKERUN")
 echo "sleeping 20s..."
 sleep 20s
 
-echo $DOCKERUN
-echo local cat
-cat ./docker-mycnf
-echo docker cat to file
-docker exec -i "$CONTAINER_ID" tee /root/.my.cnf < ./docker-mycnf
-echo docker cat imported file
+docker exec -i "$CONTAINER_ID" tee /root/.my.cnf < ./docker-mycnf > /dev/null
 docker exec "$CONTAINER_ID" cat /root/.my.cnf
-
 docker exec "$CONTAINER_ID" chmod 600 /root/.my.cnf
 
-echo == docker my.cnf ==
-docker exec "$CONTAINER_ID" cat /root/.my.cnf
-
 docker ps --all
-
-echo == ls $(pwd) ==
-ls $(pwd)
-
-echo == docker ls ==
-docker exec -t "$CONTAINER_ID" ls
-
-echo == docker testing ==
-docker exec -t "$CONTAINER_ID" mkdir -p /testing
-docker exec -t "$CONTAINER_ID" ls /testing
-
-docker exec "$CONTAINER_ID" bash -c "echo 'show processlist' | mysql"
-docker exec "$CONTAINER_ID" bash -c "echo 'show databases' | mysql"
-
 
 RETURN=1
 
@@ -65,8 +34,7 @@ for i in $(/usr/bin/find "${FIND_DIR}" -iname '*.sh')
 do
   BASENAME=$(basename $i)
   cat "$i"
-  cat "$i" | docker exec "$CONTAINER_ID" bash -c "cat > /testing/${BASENAME}"
-  docker exec "$CONTAINER_ID" cat "/testing/${BASENAME}"
+  docker exec -i "$CONTAINER_ID" tee "/testing/${BASENAME}" < "$i" > /dev/null
   docker exec "$CONTAINER_ID" bash "/testing/${BASENAME}"
 
   if [ "$?" -eq 0 ] && [ "$RETURN" -ne 2 ];
